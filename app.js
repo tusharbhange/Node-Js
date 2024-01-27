e2b3d9a2355b0241f7d2e6c936c7483ebbcae3ad
tusharbhange <tushar_bhange@yahoo.com>
2024-01-27 09:48:02 +0530

    Rectified the error - Now Working

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    Response.writeHead(200,{'Content-type': 'text/plain'});
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-type': 'text/plain'});
 
-    console.log('My name is Tushar')
+    console.log('My name is Tushar');
 
-    Response.end('My name is Tushar')
+    res.end('My name is Tushar');
 });
 
-server.listen(4000,()=>{
-    console.log('Port is 4000: ');
+server.listen(4000, () => {
+    console.log('Port is 4000');
 });
