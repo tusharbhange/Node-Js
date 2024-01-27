ed30e41d1f968dbe5670ea6aff9d17a9b37f2c55
tusharbhange <tushar_bhange@yahoo.com>
2024-01-27 09:44:02 +0530

    Created server at 4000 and printed Name

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    Response.writeHead(200,{'Content-type': 'text/plain'});
+
+    console.log('My name is Tushar')
+
+    Response.end('My name is Tushar')
 });
 
-server.listen(3000);
+server.listen(4000,()=>{
+    console.log('Port is 4000: ');
+});
